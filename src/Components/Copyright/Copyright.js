a8acae079fead432a4d60fd82c46cb65a36007fd
Kartikay Gupta <kartikay.gupta2002@gmail.com>
2023-06-20 10:47:09 +0530

    few corrections in media query and content

---
+++ src/Components/Copyright/Copyright.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import classes from './Copyright.module.css'
+const Copyright = () => {
+  return (
+    <div className={classes.copyright}>
+      &#169; UnfilteredMe 2023
+    </div>
+  )
+}
+
+export default Copyright
